3d34ecde3d107406b742fb800c3881cf2bbfd1c9
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-11-27 02:22:47 -0800

    VISUAL STUDIO HTML SUBMISSION ASSIGNMENT

--- myWebApplication/wwwroot/js/site.js
+++ myWebApplication/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+// JavaScript source code
+
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        pos++;
+        elem.style.left = pos%1150 + 'px'
+        /*
+        if (pos == 1200) {
+            //clearInterval(id);
+        } else {
+            pos++;
+            //elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+        */
+    }
+}
